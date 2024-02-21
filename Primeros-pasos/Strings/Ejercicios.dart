199b9ed73c3b585d8bf7f0bb969ea44cb3cd8234
Juanma Carmona <juanmacarmona97@gmail.com>
2024-02-21 01:41:44 +0100

    finalizo ejercicios String

--- Primeros-pasos/Strings/Ejercicios.dart
+++ Primeros-pasos/Strings/Ejercicios.dart
@@ -1,11 +1,12 @@
+
 import 'dart:io';
 
 void main(List<String> args) {
   //ejercicio1();
   //ejercicio2();
-  ejercicio3();
+  //ejercicio3();
   //ejercicio4();
-  //ejercicio5();
+  ejercicio5();
 }
 
 //1. Crea un programa que pida una cadena de texto por teclado y luego muestre cada  palabra de la cadena en una línea distinta.
@@ -61,14 +62,47 @@ void ejercicio3(){
 
 
 }
-//4. Crea un  programa que muestre por  pantalla cuantas vocales de  cada  tipo hay (cuantas ‘a’,cuantas  ‘e’,  etc.)   en   una   frase   introducida   por   teclado.   No   se   debe   diferenciar  entremayúsculas y minúsculas. Por ejemplo dada la frase “Mi mama me mima” dirá que hay:Nº de A's: 3 Nº de E's: 1 Nº de I's: 2 Nº de O's: 0 Nº de U's: 0
+//4. Crea un  programa que muestre por  pantalla cuantas vocales de  cada  tipo hay (cuantas ‘a’,cuantas  ‘e’,  etc.)   en   una   frase   introducida   por   teclado.   \
+//No   se   debe   diferenciar  entremayúsculas y minúsculas. Por ejemplo dada la frase “Mi mama me mima” dirá que hay:Nº de A's: 3 Nº de E's: 1 Nº de I's: 2 Nº de O's: 0 Nº de U's: 0
 void ejercicio4(){
 
+  int contadorA = 0;
+  int contadorE = 0;
+  int contadorI = 0;
+  int contadorO = 0;
+  int contadorU = 0;
+
+  print("Introduce una cadena de texto");
+  String texto = stdin.readLineSync() ?? "";
+
+  for(int i=0;i<texto.length;i++){
+    String caracter = texto.substring(i,i+1).toUpperCase();
+    switch(caracter){
+      case "A": contadorA++;
+      case "E": contadorE++;
+      case "I": contadorI++;
+      case "O": contadorO++;
+      case "U": contadorU++;
+    }
+  }
+  print("Num de A: $contadorA , num de E: $contadorE , num de I: $contadorI , num de O: $contadorO , num de u: $contadorU");
 }
-//5. Realiza un programa que lea una frase por teclado e indique si la frase es un palíndromo ono (ignorando espacios y sin diferenciar entre mayúsculas y minúsculas). Supondremos  queel  usuario   solo   introducirá   letras  y   espacios  (ni   comas,   ni   puntos,  ni   acentos,  etc.).  
+//5. Realiza un programa que lea una frase por teclado e indique si la frase es un palíndromo ono (ignorando espacios y sin diferenciar entre mayúsculas y minúsculas). 
+//Supondremos  que el  usuario   solo   introducirá   letras  y   espacios  (ni   comas,   ni   puntos,  ni   acentos,  etc.).  
 //Un palíndromo es un texto que se lee igual de izquierda a derecha que de derecha a  izquierda.Por ejemplo:
 //Amigo no gima 
 //Dabale arroz a la zorra el abad 
 void ejercicio5(){
-
+  print("Introduzca una frase");
+  String inTeclado = stdin.readLineSync() ?? "";
+  String frase = inTeclado.toLowerCase().replaceAll(" " , "");
+  String fraseInversa = "";
+  for(int i = frase.length;i>0;i--){
+    fraseInversa += frase.substring(i-1,i);
+  }
+  if(frase == fraseInversa){
+    print("Es un palindromo");
+  }else{
+    print("No es un palindromo");
+  }
 }
